a32c6925214af7ae0b05b277b1bc5cb572924e18
jbachurski <kbachurski@gmail.com>
2022-11-23 01:47:06 +0000

    Add C++ build commands. Add extended IO from KTL.

--- content/contest/.bashrc
+++ content/contest/.bashrc
@@ -1,3 +1,8 @@
-alias c='g++ -Wall -Wconversion -Wfatal-errors -g -std=c++14 \
-	-fsanitize=undefined,address'
-xmodmap -e 'clear lock' -e 'keycode 66=less greater' #caps = <>
+set CW='-Wall -Wextra -pedantic -Wshadow -Winit-self -Wformat=2
+  -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2
+  -Wduplicated-cond -Wcast-qual -Wcast-align'
+alias safe-c++='g++ -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
+  $CW -g3 -fsanitize=address,undefined -fstack-protector'
+alias debug-c++='g++ $CW -g3 -Og'
+alias fast-c++='g++ $CW -O3'
+#xmodmap -e 'clear lock' -e 'keycode 66=less greater' #caps = <>
---
+++ content/various/ExtIO.h
@@ -0,0 +1,42 @@
+/**
+ * Author: jbachurski
+ * License: CC0
+ * Source: Own work
+ * Description: Utilities for printing STL types.
+ * Status: debug utility
+ */
+#pragma once
+
+template<typename T1, typename T2>
+ostream& operator<<(ostream& out, const std::pair<_1, _2>& p)
+    { return out << "[" << p.first << ", " << p.second << "]"; }
+
+template<typename C>
+ostream& _print_ct(ostream& out, const C& c)
+{
+    if(c.begin() == c.end()) return out << "{}";
+    auto it = c.begin(); out << "{" << *it;
+    while(++it != c.end()) out << ", " << *it;
+    return out << "}";
+}
+
+template<typename C,typename = typename C::iterator,
+  typename=typename<enable_if<!is_same<C,string>::value>::type>
+ostream& operator<<(ostream& out, const C& c)
+    { return _print_ct(out, c); }
+
+
+template<typename... Args, size_t... I>
+void _print_tup(ostream& out, const std::tuple<Args...>& t,
+    std::index_sequence<I...>)
+{
+    (..., (out << (I ? ", " : "") << get<I>(t)));
+}
+
+template<typename... Args>
+ostream& operator<<(ostream& out, const std::tuple<Args...>& t)
+{
+    out << '['; _print_tup(out, t,
+        std::make_index_sequence<sizeof...(Args)>());
+    return out << ']';
+}
